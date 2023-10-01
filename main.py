04bed59b229cec222895f988e6c496fb67b6ec3e
Nicolas Paiva <nipaiva93@gmail.com>
2023-10-01 14:31:58 -0300

    versao 0.1

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello world!")
+#!/bin/env python3
+"""Hello world Multi Linguas
+    programa que mostra hello world!
+    ira mostrar hello world de acordo com a lingua do ambiente configurado
+
+como usar:
+    tenha a variavel LANG configurado no seus sistema linux
+    exemplo:
+        export LANG=pt_BR.UTF-8
+
+execuçao:
+    python3 main.py
+    ou
+    ./main.py
+"""
+import os
+__version__ = "0.1"
+__author__ = "nicolas"
+msg = "hello world!"
+current_language = os.getenv("LANG", "en_US")[:5]
+if current_language == "pt_BR":
+    msg = "ola, mundo!"
+elif current_language == "it_IT":
+    msg = "ciao,mondo!";
+
+
+print(msg)
