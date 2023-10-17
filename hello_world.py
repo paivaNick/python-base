2ba6a223d4ef128a7cc4a0ef6f59f16fda4e8732
Nicolas Paiva <nipaiva93@gmail.com>
2023-10-17 14:15:44 -0300

    hello world 0.1.2

--- hello_world.py
+++ hello_world.py
@@ -8,19 +8,39 @@ como usar:
     exemplo:
         export LANG=pt_BR.UTF-8
 
+usando CLI:
+    python3 main.py --lang=pt_BR
 execuçao:
     python3 main.py
     ou
     ./main.py
 """
 import os
+import sys
 __version__ = "0.1.2"
 __author__ = "nicolas"
-
-current_language = os.getenv("LANG", "en_US")[:5]
-msg = {
-    "pt_BR": "ola mundo!",
-    "en_US": "hello world",
-    "es_SP": "hola, mundo"
+arguments = {
+    "lang": None,
+    "cout": 1  
 }
-print(msg[current_language])
+for arg in sys.argv[1:]:
+    # TODO - tratar value error
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f" opçao {key} invalida!")
+        sys.exit()
+    arguments[key] = value
+msg = {
+    "pt_BR": "ola mundo!. ",
+    "en_US": "hello world. ",
+    "es_SP": "hola, mundo. "
+}
+current_language = arguments["lang"]
+
+if current_language is None:
+    current_language = os.getenv("LANG", "en_US")[:5]
+
+
+print(msg[current_language] *  int(arguments["cout"]))
